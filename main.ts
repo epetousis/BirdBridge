4f00d4507829002b3387c784cfba1fde5e6f373f
Evan Petousis <evan@petousis.net>
2023-08-07 21:52:42 +1000

    implement bookmarks endpoint

--- main.ts
+++ main.ts
@@ -320,6 +320,35 @@ app.get('/api/v1/favourites', async (req, res) => {
     res.send(toots);
 });
 
+app.get('/api/v1/bookmarks', async (req, res) => {
+    const variables = {
+        "includeTweetImpression":true,
+        "includeHasBirdwatchNotes":false,
+        "includeEditPerspective":false,
+        "includeEditControl":true,
+        "count":req.body.limit,
+        "includeTweetVisibilityNudge":true,
+        // TODO: add pagination support via cursor
+    };
+    const features = {
+        "unified_cards_ad_metadata_container_dynamic_card_content_query_enabled": true,
+    };
+    const twreq = await req.oauth!.getGraphQL('/E-Rqts_gtMp60KgQK2Xv9A/BookmarkTimelineV2', variables, features);
+    const response = await twreq.json();
+    const tweets = response.data.timeline_response.timeline.instructions
+      .find((i) => i['__typename'] === 'TimelineAddEntries')
+      .entries
+      .map((e) => ({
+        ...e.content.content?.tweetResult.result.legacy,
+        user: e.content.content?.tweetResult.result.core.user_result.result.legacy,
+        // The GraphQL "legacy" param just straight up has the ID missing...righto
+        id_str: e.content.content?.tweetResult.result.rest_id,
+      }))
+      .filter((t) => !!t && t.user);
+    const toots = tweets.map(tweetToToot);
+    res.send(toots);
+});
+
 app.get('/api/v1/lists', async (req, res) => {
     const twreq = await req.oauth!.request(
         'GET',
