54236de7b863158f117180e3b5ea007082727e7c
Evan Petousis <evan@petousis.net>
2023-08-20 11:19:48 +1000

    add experimental block_the_blue flag

--- config.ts
+++ config.ts
@@ -6,6 +6,10 @@ export interface Config {
     consumer_secret: string,
     max_context_pages?: number,
     pagination_safety_buffer?: number,
+    /** Controls whether to block Twitter Blue users who you do not follow.
+     * Business accounts are still returned.
+     */
+    block_the_blue?: boolean,
     root: string,
     domain: string
 }
